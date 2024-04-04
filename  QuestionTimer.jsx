f0e76b8333c2cbf67642b23f2c8182b549e5b3e8
gmlgus0424 <gmlgus0424@naver.com>
2024-04-04 13:34:56 +0900

    완성

---  QuestionTimer.jsx
+++  QuestionTimer.jsx
@@ -1,28 +1,34 @@
 import { useState, useEffect } from 'react';
 
-export default function QuestionTimer({ timeout, onTimeout }) {
+export default function QuestionTimer({ timeout, onTimeout, mode }) {
   const [remainingTime, setRemainingTime] = useState(timeout);
 
   useEffect(() => {
     console.log('SETTING TIMEOUT');
-  const Timer=  setTimeout(onTimeout, timeout);
+    const timer = setTimeout(onTimeout, timeout);
 
-  return()=>{
-    clearTimeout();
-  }
+    return () => {
+      clearTimeout(timer);
+    };
   }, [timeout, onTimeout]);
 
   useEffect(() => {
     console.log('SETTING INTERVAL');
-  const interval=  setInterval(() => {
+    const interval = setInterval(() => {
       setRemainingTime((prevRemainingTime) => prevRemainingTime - 100);
     }, 100);
 
-//clean up
-     return()=>{
-      clearInterval(interval)
-     }
+    return () => {
+      clearInterval(interval);
+    };
   }, []);
 
-  return <progress id="question-time" max={timeout} value={remainingTime} />;
+  return (
+    <progress
+      id="question-time"
+      max={timeout}
+      value={remainingTime}
+      className={mode}
+    />
+  );
 }
---
+++ Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+      <h1>역사 퀴즈</h1>
+    </header>
+  )
+}
+
+export default Header
